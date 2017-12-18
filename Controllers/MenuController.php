0649607d6253b4537b082020444bcb1e4ab0111c
User <User@192.168.0.100>
2017-12-18 23:41:24 +0400

    MVC+загрузка_из_файла+валидация

--- Controllers/MenuController.php
+++ Controllers/MenuController.php
@@ -9,33 +9,5 @@ class MenuController {
        // echo 'ROOT:'.ROOT;
         require_once (ROOT.'/menu.php');
     }
-    public static $menuList;
-    public function actionRollsList(){
-     
-        self::$menuList=Menu::getMenuListByCategory('Sushi');
-        
-        require_once (ROOT.'/menu.php');
-        return self::$menuList;
-    }
-     public function actionSetsList(){
-     
-        self::$menuList=Menu::getMenuListByCategory('Sets');
-        
-        require_once (ROOT.'/menu.php');
-        return self::$menuList;
-    }
-     public function actionDrinksList(){
-     
-        self::$menuList=Menu::getMenuListByCategory('Drinks');
-        
-        require_once (ROOT.'/menu.php');
-        return self::$menuList;
-    }
-     public function actionSaladsList(){
-     
-        self::$menuList=Menu::getMenuListByCategory('Salads');
-        
-        require_once (ROOT.'/menu.php');
-        return self::$menuList;
-    }
+    
 }
--- Model/Menu.php
+++ Model/Menu.php
@@ -1,72 +1,8 @@
 <?php
 
-include_once ROOT.'/DB.php';
+
 class Menu {
 
-    public static function getMenuListByCategory($category){
-       $db=DB::getDBConnection();
-      
-       switch ($category){
-           case 'Sushi':
-               $sushiList=array();
-
-       $resList=$db->query('select * from roll');
-       $i=0;
-              while($row =$resList->fetch()){
-                   $sushiList[$i]['id']=$row['Roll_id'];
-                   $sushiList[$i]['Name']=$row['Name'];
-                   $sushiList[$i]['Price']=$row['Price'];
-                   $sushiList[$i]['pic']=$row['Roll_pic'];
-                   $i++;
-               }
-               
-               return ($sushiList);
-               break;
-               case 'Sets':
-               $sushiList=array();
-
-       $resList=$db->query('select * from sushiset');
-       $i=0;
-              while($row =$resList->fetch()){
-                   $sushiList[$i]['id']=$row['SushiSet_id'];
-                   $sushiList[$i]['Name']=$row['Name'];
-                   $sushiList[$i]['Price']=$row['Price'];
-                   $sushiList[$i]['pic']=$row['SushiSet_pic'];
-                   $i++;
-               }
-       
-               return ($sushiList);
-               break;
-               case 'Salads':
-               $sushiList=array();
-
-       $resList=$db->query('select * from salad');
-       $i=0;
-              while($row =$resList->fetch()){
-                   $sushiList[$i]['id']=$row['Salad_id'];
-                   $sushiList[$i]['Name']=$row['Name'];
-                   $sushiList[$i]['Price']=$row['Price'];
-                   $sushiList[$i]['pic']=$row['Salad_pic'];
-                   $i++;
-               }
-       
-               return ($sushiList);
-               break;
-               case 'Drinks':
-               $sushiList=array();
-
-       $resList=$db->query('select * from drink');
-       $i=0;
-              while($row =$resList->fetch()){
-                   $sushiList[$i]['id']=$row['Drink_id'];
-                   $sushiList[$i]['Name']=$row['Name'];
-                   $sushiList[$i]['Price']=$row['Price'];
-                   $sushiList[$i]['pic']=$row['Drink_pic'];
-                   $i++;
-               }
-       
-               return ($sushiList);
-               break;
-       }
-    }
+    
+    
 }
